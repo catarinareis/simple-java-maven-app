2cb73bfeee56418153bd53ea7e9a5a041a3ecb97
Catarina I. Reis <catarina.isabel.reis@gmail.com>
2018-01-31 14:52:39 +0000

    Updated test to comply with new message

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hola Portugal!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hola Equador!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
