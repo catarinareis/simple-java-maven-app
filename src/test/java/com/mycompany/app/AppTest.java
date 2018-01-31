cf8ed095734a486c2c1642aa6a5179d191ba372e
Catarina I. Reis <catarina.isabel.reis@gmail.com>
2018-01-31 22:24:48 +0000

    Updated test to comply with new message

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hola Equador!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hola Equador123!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hola Equador!\"");
+            fail("\"message\" is not \"Hola Equador123!\"");
         }
     }
 
